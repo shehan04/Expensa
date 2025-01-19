523f6b3fd10470187fb6514283173557cc7c5f2c
Shehan Silva <shehan04@outlook.com>
2025-01-19 22:46:52 +0530

    latest changes

--- src/Services/Expenses/Expenses.API/Program.cs
+++ src/Services/Expenses/Expenses.API/Program.cs
@@ -25,8 +25,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
 
 app.Run();
